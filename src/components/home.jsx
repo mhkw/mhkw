3169a0c35cd0d942b64b1709b7ebc7b14b918d22
BingYuanLuoLei <1145235349@qq.com>
2018-01-08 09:06:44 +0800

    提交font

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { List, InputItem, Toast, Button, WhiteSpace, Checkbox, Modal,NavBar,Icon } from 'antd-mobile';
+import { List, InputItem, Toast, Button, WhiteSpace, Checkbox, Modal, NavBar, Icon, Tabs } from 'antd-mobile';
 import {Link} from 'react-router';
 import { createForm } from 'rc-form';
 import QueueAnim from 'rc-queue-anim';
@@ -7,47 +7,45 @@ import QueueAnim from 'rc-queue-anim';
 import '../css/font/iconfont.css'
 
 const loginUrl = [
-    require('../images/login_logo.png'),
-    require('../images/login_phone.png'),
-    require('../images/login_psd.png')
+    require('../images/home/lei1.png'),
+    require('../images/home/lei2.png'),
+    require('../images/home/lei3.png'),
+    require('../images/home/lei4.png'),
+    require('../images/home/lei5.png'),
+    require('../images/home/lei6.png'),
+    require('../images/home/lei7.png'),
+    require('../images/home/lei8.png'),
+    require('../images/touxiang.png'),
 ]
 
 const AgreeItem = Checkbox.AgreeItem;
 const prompt = Modal.prompt;
 
-class LoginView extends React.Component {
-    componentDidMount (){
-    
-    }
+export default class LoginView extends React.Component {
     state = {
         show: true,
         type: 'money',
         hasError: false,
         value: '',
-        maskClosable: true
+        maskClosable: true,
+        data: ['', '', ''],
+        // imgHeight: 80,
     };
-    onErrorClick = () => {
-        if (this.state.hasError) {
-            Toast.info('请输入11位手机号！');
-        }
-    }
-    onChange = (value) => {
-        if (value.replace(/\s/g, '').length < 11) {
-            this.setState({
-                hasError: true,
-            });
-        } else {
-            this.setState({
-                hasError: false,
-            });
-        }
-        this.setState({
-            value,
-        });
+    componentDidMount() {
+        
     }
     render() {
-        const { getFieldProps } = this.props.form;
-        const { type } = this.state;
+        const tabs = [
+            { title: <div className="fn-clear tabsList"><img src={loginUrl[0]} /><p>软件开发</p></div>},
+            { title: <div className="fn-clear tabsList"><img src={loginUrl[1]} /><p>软件开发</p></div>},
+            { title: <div className="fn-clear tabsList"><img src={loginUrl[2]} /><p>软件开发</p></div>},
+            { title: <div className="fn-clear tabsList"><img src={loginUrl[3]} /><p>软件开发</p></div>},
+            { title: <div className="fn-clear tabsList"><img src={loginUrl[4]} /><p>软件开发</p></div>},
+            { title: <div className="fn-clear tabsList"><img src={loginUrl[5]} /><p>软件开发</p></div>},
+            { title: <div className="fn-clear tabsList"><img src={loginUrl[6]} /><p>软件开发</p></div>},
+            { title: <div className="fn-clear tabsList"><img src={loginUrl[7]} /><p>软件开发</p></div>},
+        ];
+
         return (
             <div className="homeWrap">
                 <div className="homeWrapTop">
@@ -56,20 +54,38 @@ class LoginView extends React.Component {
                             mode="light"
                             onLeftClick={() => hashHistory.goBack()}
                             rightContent={[
-                                <Icon key="0" type="search" style={{ marginRight: '16px' }} />
+                                <div className="searchAll">
+                                    <i className="iconfont icon-icon05"></i>
+                                </div>
                             ]}
-                        ><Link to="/city"><i className="iconfont"></i>山景路666号</Link></NavBar>
+                        ><Link to="/city"><i className="iconfont icon-dingwei"></i>山景路666号</Link></NavBar>
+                    </div>
+                    <div className="hometabs"> 
+                        <Tabs tabs={tabs} 
+                            tabBarBackgroundColor="transparent"
+                            tabBarUnderlineStyle={{display:"none"}}
+                            onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}
+                        >
+                            
+                        </Tabs>
                     </div>
                 </div>
                 <div className="homeWrapMain">
+                    <div className="items">
+                        <div className="itemsTop">
+                            <div className="itemsTopPic">
+                                <img src={} alt=""/>
+                            </div>
+                            <div className="itemsTopRight">
 
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
     }
 }
-const H5NumberInputExampleWrapper = createForm()(LoginView);
-export default H5NumberInputExampleWrapper;
 
 
 
