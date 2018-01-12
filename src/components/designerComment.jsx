fae8239effa8896dd9b012e6819354fc7238f146
Zheng Shuai <864737869@qq.com>
2018-01-12 19:39:42 +0800

    增加图片灯箱

--- src/components/designerComment.jsx
+++ src/components/designerComment.jsx
@@ -5,82 +5,31 @@ import { Toast, NavBar, Icon, Flex, TextareaItem } from 'antd-mobile';
 import QueueAnim from 'rc-queue-anim';
 import axios from 'axios';
 
+import PhotoSwipeItem from './photoSwipeElement.jsx';
+import CommentItem from './CommentItem.jsx';
+
+import '../js/photoswipe/photoswipe.css';
+import '../js/photoswipe/default-skin/default-skin.css';
+import PhotoSwipe  from '../js/photoswipe/photoswipe.min.js';
+import PhotoSwipeUI_Default from '../js/photoswipe/photoswipe-ui-default.min.js';
+
+let openPhotoSwipe = function (items, index) {
+    let pswpElement = document.querySelectorAll('.pswp')[0];
+    let options = {
+        index: index,
+        showAnimationDuration: 100,
+        hideAnimationDuration: 100
+    }
+    let gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+}
+
 const imgUrl = {
     userImg: require("../images/avatar.png"),
     dishPic: [require("../images/banner01.jpg"), require("../images/banner02.jpg"), require("../images/banner03.jpg"), require("../images/avatar.png"), require("../images/homePic.png")]
 }
 
-const CommentItem = (props) => (
-    <div className="comment-item">
-        <div className="user-pic">
-            <img src={props.avatarImg} className="user-img"/>
-        </div>
-        <div className="comment-top">
-            <div className="user-name">{props.nick_name}</div>
-            <div className="info">
-                <span className="scoreTitle">评分</span>
-                <i className={ props.score > 0 ? "iconfont icon-wujiaoxing light" : "iconfont icon-wujiaoxing"} />
-                <i className={ props.score > 1 ? "iconfont icon-wujiaoxing light" : "iconfont icon-wujiaoxing"} />
-                <i className={ props.score > 2 ? "iconfont icon-wujiaoxing light" : "iconfont icon-wujiaoxing"} />
-                <i className={ props.score > 3 ? "iconfont icon-wujiaoxing light" : "iconfont icon-wujiaoxing"} />
-                <i className={ props.score > 4 ? "iconfont icon-wujiaoxing light" : "iconfont icon-wujiaoxing"} />
-            </div>
-        </div>
-        <div className="context">
-            <p className="text">更新一组影吧风格，吉茶屋  用原创个性字体进行组合设计。更新一组影吧风格，吉茶屋  用原创个性字体进行组合设计。</p>
-            <span className="get-all">更多</span>
-        </div>
-        <div className="dishPic">
-        {
-            props.dishPic.map((imgUrl) => (
-                <div className="imgFrame"><img src={imgUrl}/></div>
-            ))
-        }
-        </div>
-        <div className="bottom-box">
-            <span className="time">{props.time}</span>
-            <div className="comment-icon">
-                <span className="box-collect">
-                    <i className="iconfont icon-xin-1"></i>
-                    <span className="number">1</span>
-                </span>
-                <span className="dashed-arrow"></span>
-                <span className="box-comment">
-                    <i className="iconfont icon-icon-talk"></i>
-                    <span className="number">0</span>
-                </span>
-            </div>
-        </div>
-        <div className="comment-like-box clearfix">
-            <i className="iconfont icon-xin-1"></i>
-            <ul className="like-user">
-                <li>hkw151541,</li>
-                <li>晓风残月,</li>
-                <li>晓风残月,</li>
-                <li>晓风残月,</li>
-                <li>晓风残月,</li>
-                <li>晓风残月,</li>
-                <li>晓风残月,</li>
-                <li>方少言</li>
-            </ul>
-            <span className="txt">觉得很赞</span>
-        </div>
-        <div className="comment-reply-box">
-            <div className="reply-line"
-                onClick={(e) => { props.onActive(props.index, e)}}
-            >
-                <span className="origin">方少言:</span>
-                <p className="txt">无敌赞</p>
-            </div>
-            <div className="reply-line" 
-                onClick={(e) => { props.onActive(props.index, e) }}
-            >
-                <span className="origin">方少言 回复 晓风残月:</span>
-                <p className="txt">芭芭拉啦啦把芭芭拉啦啦666666啦啦啦啦啦8888888</p>
-            </div>
-        </div>
-    </div>
-)
+
 
 export default class DesignerComment extends React.Component {
     constructor(props) {
@@ -124,7 +73,7 @@ export default class DesignerComment extends React.Component {
     }
     //回复输入框失去焦点
     onBlurReplyInput = (e) => {
-        // this.setState({ showReplyInput: false }); 
+        this.setState({ showReplyInput: false }); 
     }
     //点击发送按钮
     onTouchSend= () => {
@@ -145,6 +94,18 @@ export default class DesignerComment extends React.Component {
             }, this.handleSend, false, "get");       
         }
     }
+    onTouchImg = (index, dishPic) => {
+        // console.log(dishPic);
+        let items = [];
+        dishPic.map((value)=>{
+            let item = {};
+            item.src = value;
+            item.w = 1242;
+            item.h = 298;
+            items.push(item);
+        })
+        openPhotoSwipe(items, index)
+    }
     render() {
         return (
             <div className="designerComment">
@@ -156,6 +117,7 @@ export default class DesignerComment extends React.Component {
                     time={"11月08日"} 
                     index="1"
                     onActive={this.onTouchReply}
+                    TouchImg={this.onTouchImg}
                 />
                 <CommentItem
                     avatarImg={imgUrl.userImg}
@@ -165,6 +127,7 @@ export default class DesignerComment extends React.Component {
                     time={"11月08日"}
                     index="2"
                     onActive={this.onTouchReply}
+                    TouchImg={this.onTouchImg}
                 />
                 <div className="popup-comment-input-box"
                     // style={{ "display": this.state.showReplyInput ? "block" : "none" }}
@@ -191,6 +154,7 @@ export default class DesignerComment extends React.Component {
                         onTouchStart={this.onTouchSend}
                     >发送</span>
                 </div>
+                <PhotoSwipeItem/>
             </div>
         )
     }
