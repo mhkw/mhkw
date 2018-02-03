dafa6ea9589b74e8de12de874d66d3605e9a35a3
Zheng Shuai <864737869@qq.com>
2018-02-03 18:10:33 +0800

    没写啥

--- src/components/CreateOffer.jsx
+++ src/components/CreateOffer.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { NavBar, Icon, Button, WingBlank, Flex, InputItem} from 'antd-mobile';
+import { NavBar, Icon, Button, WingBlank, Flex, InputItem, DatePicker, List} from 'antd-mobile';
 import { hashHistory, Link } from 'react-router';
 
 const OfferItem = (props) => (
     <div className="offer-item">
         <Flex justify="between" align="start">
-            <Flex.Item className="title" style={{ "flex": "3" }}>{props.title}</Flex.Item>
+            <Flex.Item className="title" style={{ "flex": "2" }}>{props.title}</Flex.Item>
             <Flex.Item className="price">{props.price}</Flex.Item>
         </Flex>
         <Flex justify="between" align="start">
@@ -21,7 +21,30 @@ export default class CreateOffer extends React.Component {
         this.state = {
             pure_price: "3560.00",
             remarks: "",
+            customer_name: "",
+            customer_company: "",
+            customer_phone: "",
+            project_name: "",
+            cut_off_date: "",
+            cut_off_day: "",
         }
+    }
+    onOkDatePicker(date) {
+        let year = date.getFullYear();
+        let month = date.getMonth() < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
+        let day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+        let formatDate = `${year}-${month}-${day}`;
+        
+        let nowTime = (new Date()).getTime(); //现在的时间
+        let curTime = (new Date(date)).getTime(); //选择的时间
+        let cut_off_day = Math.ceil( (curTime - nowTime) / (1000 * 60 * 60 * 24) );
+        this.setState({ 
+            cut_off_date: formatDate,
+            cut_off_day: cut_off_day
+        });
+    }
+    onClickCreateOffer() {
+        console.log("click create offer")
     }
     render() {
         return (
@@ -58,10 +81,95 @@ export default class CreateOffer extends React.Component {
                         <span className="right-sum-price">5632.00</span>
                     </div>
                     <InputItem
-                        placeholder="auto focus"
+                        className="remarks-input-create-offer"
+                        type="string"
+                        placeholder="添加备注"
                         value={this.state.remarks}
-                        onChange={(v) => { this.setState({ remarks: v})}}
-                    >标题</InputItem>
+                        onChange={(val) => { this.setState({ remarks: val }) }}
+                    ><i className="iconfont icon-bianji"></i></InputItem>
+                </div>
+                <div className="create-offer-customer">
+                    <p className="customer-title">
+                        <span className="left">客户信息</span>
+                        <span className="right">请输入客户信息</span>
+                        <i className="iconfont icon-geren3"></i>
+                    </p>
+                    <div className="customer-input-box">
+                        <InputItem
+                            className="input-name"
+                            type="string"
+                            maxLength="10"
+                            placeholder=""
+                            value={this.state.customer_name}
+                            onChange={(val) => { this.setState({ customer_name: val }) }}
+                        >姓名</InputItem>
+                        <InputItem
+                            className="input-company"
+                            type="string"
+                            maxLength="30"
+                            placeholder="用于身份确认"
+                            value={this.state.customer_company}
+                            onChange={(val) => { this.setState({ customer_company: val }) }}
+                        >公司</InputItem>
+                        <InputItem
+                            className="input-phone"
+                            type="number" 
+                            maxLength="11"
+                            placeholder="输入手机号用于接收短信"
+                            value={this.state.customer_phone}
+                            onChange={(val) => { this.setState({ customer_phone: val }) }}
+                        >手机</InputItem>
+                    </div>
+                </div>
+                <div className="create-offer-project">
+                    <p className="project-title">
+                        <span className="left">项目信息</span>
+                        <span className="right">请输入项目信息</span>
+                    </p>
+                    <div className="customer-input-box project-input">
+                        <InputItem
+                            className="input-project-name"
+                            type="string"
+                            maxLength="20"
+                            placeholder=""
+                            value={this.state.project_name}
+                            onChange={(val) => { this.setState({ project_name: val }) }}
+                        >项目名称</InputItem>
+                        <DatePicker
+                            className="cut-off-date"
+                            mode="date"
+                            title="请选择截止日期(至少5天)"
+                            // extra={<span>报价在<span className="num">{this.state.cut_off_date}</span>天后自动截止</span>}
+                            // extra="请选择截止日期"
+                            extra={this.state.cut_off_day ? `报价在${this.state.cut_off_day}天后自动截止` : "请选择截止日期"}
+                            format={date => `报价在${this.state.cut_off_day}天后自动截止`}
+                            // value={this.state.date}
+                            value={this.state.date}
+                            onChange={date => this.setState({ date })}
+                            onOk={date => this.onOkDatePicker(date)}
+                            minDate={new Date((new Date()).getTime() + (5 * 24 * 60 * 60 * 1000))}
+                        >
+                            <List.Item className="cut_off_date-item" arrow="horizontal">截止日期</List.Item>
+                        </DatePicker>
+                    </div>
+                </div>
+                <div className="create-offer-foot">
+                    <div className="discount">
+                        <span className="unit">
+                            <span className="title">总计:</span>
+                            <span className="price">5632.00</span>
+                        </span>
+                        <span className="unit">
+                            <span className="title">优惠后:</span>
+                            <span className="price">5000.00</span>
+                        </span>
+                        <span className="tax-unit">(含3%税票)</span>
+                    </div>
+                    <Button
+                        onClick={this.onClickCreateOffer}
+                        className="create-offer-button"
+                        activeClassName="create-offer-button-active"
+                    >生成报价单</Button>
                 </div>
             </div>
         )
